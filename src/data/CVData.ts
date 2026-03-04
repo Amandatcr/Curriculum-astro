ff5aa6b872a8cc2117a7647f6209f5345530c1d5
Amanda Cruz <tacr95@educa.madrid.org>
2026-03-04 20:06:23 +0100

    Modificación usando componentes y Tailwind

---
+++ src/data/CVData.ts
@@ -0,0 +1,21 @@
+export const conocimientosTecnicos = [
+  "HTML5, CSS3, JavaScript (bases)",
+  "PHP (bases), MySQL",
+  "Excel avanzado",
+  "A3ERP, SAGE",
+  "SAGE (Contaplus, Nominaplus)"
+];
+
+export const actitudes = [
+  "Comunicación efectiva y habilidades interpersonales",
+  "Proactividad e iniciativa",
+  "Capacidad de análisis y resolución de problemas",
+  "Organización y gestión eficiente del tiempo",
+  "Orientación al cliente",
+  "Persuasión y negociación"
+];
+
+export const masInformacion = [
+  "Carnet de conducir tipo B",
+  "Vehículo propio"
+]
